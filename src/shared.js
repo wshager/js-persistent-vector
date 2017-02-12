425f61071e24d5fab6dea362aa7cea16abb07fd5
W.S. Hager <wshager@gmail.com>
2017-02-12 21:27:32 +0100

    moved out slice

--- src/shared.js
+++ src/shared.js
@@ -29,10 +29,10 @@ function pushLeaf(shift, i, root, tail, copyFn, newTopFn, id) {
 }
 
 export function tailSize(vec) {
-	return !vec.size ? 0 : ((vec.size - 1) & MASK) + 1;
+	return !vec.capacity ? 0 : ((vec.capacity - 1) & MASK) + 1;
 }
 
-export function wrapIndex(iter, index) {
+export function wrapIndex(vec, index) {
   // This implements "is array index" which the ECMAString spec defines as:
   //
   //     A String property name P is an array index if and only if
@@ -47,26 +47,34 @@ export function wrapIndex(iter, index) {
     }
     index = uint32Index;
   }
-  return index < 0 ? ensureSize(iter) + index : index;
+  return index < 0 ? ensureSize(vec) + index : index;
 }
 
 export function get(vec, i) {
-    if(i < vec.size) {
-        var node = vecNodeFor(vec,i + vec.origin);
+    if(i >= 0 && i < vec.size) {
+        i += vec.origin;
+        var node = vecNodeFor(vec,i);
         return node && node[i & MASK];
     }
 }
 
-export function* iter(vec){
-	for(var i = vec.origin;i<vec.size;i++) {
+export function* values(vec){
+	for(var i = 0;i<vec.size;i++) {
 		yield get(vec,i);
+	}
+}
+
+export function* entries(vec){
+	for(var i = 0;i<vec.size;i++) {
+		yield [i,get(vec,i)];
 	}
 }
 
 export function set(vec, i, val, update, updateTail, copyFn) {
     if (i > vec.size) throw new Error("Index out of bounds. Index :" + i + ", Size:" + vec.size);
     if(i == vec.size) return vec.push(val);
-    if (i >= tailOffset(vec.size)) {
+    i += vec.origin;
+    if (i >= tailOffset(vec.capacity)) {
         return update(vec, vec.origin, vec.capacity, vec.shift, vec.root, updateTail(vec, i & MASK, val));
     }
     else {
@@ -169,48 +177,8 @@ export function tailOffset(size) {
 	return (size - 1) & (~MASK);
 }
 
-export function slice(vec, begin, end, update) {
-  var size = vec.size;
-  if (wholeSlice(begin, end, size)) {
-    return vec;
-  }
-  return setListBounds(
-    vec,
-    resolveBegin(begin, size),
-    resolveEnd(end, size),
-    update
-  );
-}
 
-export function wholeSlice(begin, end, size) {
-  return (begin === 0 || (size !== undefined && begin <= -size)) &&
-    (end === undefined || (size !== undefined && end >= size));
-}
-
-export function resolveBegin(begin, size) {
-  return resolveIndex(begin, size, 0);
-}
-
-export function resolveEnd(end, size) {
-  return resolveIndex(end, size, size);
-}
-
-function resolveIndex(index, size, defaultIndex) {
-  return index === undefined ?
-    defaultIndex :
-    index < 0 ?
-      Math.max(0, size + index) :
-      size === undefined ?
-        index :
-        Math.min(size, index);
-}
-
-
-function newId() {
-    return {};
-}
-
-function vecNodeFor(vec,i){
+export function vecNodeFor(vec,i){
     if (i >= tailOffset(vec.capacity)) {
         return vec.tail;
     } else {
@@ -220,183 +188,4 @@ function vecNodeFor(vec,i){
         }
         return node;
     }
-}
-
-function setListBounds(vec, begin, end, update) {
-  // Sanitize begin & end using this shorthand for ToInt32(argument)
-  // http://www.ecma-international.org/ecma-262/6.0/#sec-toint32
-  if (begin !== undefined) {
-    begin = begin | 0;
-  }
-  if (end !== undefined) {
-    end = end | 0;
-  }
-  var owner = vec.id || newId();
-  var oldOrigin = vec.origin;
-  var oldCapacity = vec.capacity;
-  var newOrigin = oldOrigin + begin;
-  var newCapacity = end === undefined ? oldCapacity : end < 0 ? oldCapacity + end : oldOrigin + end;
-  if (newOrigin === oldOrigin && newCapacity === oldCapacity) {
-    return vec;
-  }
-
-  // If it's going to end after it starts, it's empty.
-  if (newOrigin >= newCapacity) {
-    return vec.clear();
-  }
-
-  var newLevel = vec.shift;
-  var newRoot = vec.root;
-
-  // New origin might need creating a higher root.
-  var offsetShift = 0;
-  while (newOrigin + offsetShift < 0) {
-    newRoot = ensureEditable(newRoot && newRoot.length ? [undefined, newRoot] : [], owner);
-    newLevel += BITS;
-    offsetShift += 1 << newLevel;
-  }
-  if (offsetShift) {
-    newOrigin += offsetShift;
-    oldOrigin += offsetShift;
-    newCapacity += offsetShift;
-    oldCapacity += offsetShift;
-  }
-
-  var oldTailOffset = tailOffset(oldCapacity);
-  var newTailOffset = tailOffset(newCapacity);
-
-  // New size might need creating a higher root.
-  while (newTailOffset >= 1 << (newLevel + BITS)) {
-    newRoot = ensureEditable(newRoot && newRoot.length ? [newRoot] : [], owner);
-    newLevel += BITS;
-  }
-
-  // Locate or create the new tail.
-  var oldTail = vec.tail;
-  var newTail = newTailOffset < oldTailOffset ?
-    vecNodeFor(vec, newCapacity - 1) :
-    newTailOffset > oldTailOffset ? newTopFn([], owner) : oldTail;
-
-  // Merge Tail into tree.
-  if (oldTail && newTailOffset > oldTailOffset && newOrigin < oldCapacity && oldTail.length) {
-    newRoot = ensureEditable(newRoot, owner);
-    var node = newRoot;
-    for (var level = newLevel; level > BITS; level -= BITS) {
-      var idx = (oldTailOffset >>> level) & MASK;
-      node = node[idx] = ensureEditable(node[idx], owner);
-    }
-    node[(oldTailOffset >>> BITS) & MASK] = oldTail;
-  }
-
-  // If the size has been reduced, there's a chance the tail needs to be trimmed.
-  if (newCapacity < oldCapacity) {
-    newTail = newTail && removeAfter(newTail,owner, 0, newCapacity);
-  }
-
-  // If the new origin is within the tail, then we do not need a root.
-  if (newOrigin >= newTailOffset) {
-    newOrigin -= newTailOffset;
-    newCapacity -= newTailOffset;
-    newLevel = BITS;
-    newRoot = null;
-    newTail = newTail && removeBefore(newTail,owner, 0, newOrigin);
-
-  // Otherwise, if the root has been trimmed, garbage collect.
-  } else if (newOrigin > oldOrigin || newTailOffset < oldTailOffset) {
-    offsetShift = 0;
-
-    // Identify the new top root node of the subtree of the old root.
-    while (newRoot) {
-      var beginIndex = (newOrigin >>> newLevel) & MASK;
-      if (beginIndex !== (newTailOffset >>> newLevel) & MASK) {
-        break;
-      }
-      if (beginIndex) {
-        offsetShift += (1 << newLevel) * beginIndex;
-      }
-      newLevel -= BITS;
-      newRoot = newRoot[beginIndex];
-    }
-
-    // Trim the new sides of the new root.
-    if (newRoot && newOrigin > oldOrigin) {
-      newRoot = removeBefore(newRoot,owner, newLevel, newOrigin - offsetShift);
-    }
-    if (newRoot && newTailOffset < oldTailOffset) {
-      newRoot = removeAfter(newRoot,owner, newLevel, newTailOffset - offsetShift);
-    }
-    if (offsetShift) {
-      newOrigin -= offsetShift;
-      newCapacity -= offsetShift;
-    }
-  }
-
-  return update(vec, newOrigin, newCapacity, newLevel, newRoot, newTail);
-}
-
-function removeBefore(node,ownerID, level, index) {
-    if (index === level ? 1 << level : 0 || node.length === 0) {
-      return node;
-    }
-    var originIndex = (index >>> level) & MASK;
-    if (originIndex >= node.length) {
-      return newTopFn(ownerID);
-    }
-    var removingFirst = originIndex === 0;
-    var newChild;
-    if (level > 0) {
-      var oldChild = node[originIndex];
-      newChild = oldChild && removeBefore(oldChild,ownerID, level - BITS, index);
-      if (newChild === oldChild && removingFirst) {
-        return node;
-      }
-    }
-    if (removingFirst && !newChild) {
-      return node;
-    }
-    var editable = ensureEditable(node, ownerID);
-    if (!removingFirst) {
-      for (var ii = 0; ii < originIndex; ii++) {
-        editable[ii] = undefined;
-      }
-    }
-    if (newChild) {
-      editable[originIndex] = newChild;
-    }
-    return editable;
-}
-
-function removeAfter(node,ownerID, level, index) {
-    if (index === (level ? 1 << level : 0) || node.length === 0) {
-      return node;
-    }
-    var sizeIndex = ((index - 1) >>> level) & MASK;
-    if (sizeIndex >= node.length) {
-      return node;
-    }
-
-    var newChild;
-    if (level > 0) {
-      var oldChild = node[sizeIndex];
-      newChild = oldChild && removeAfter(oldChild,ownerID, level - BITS, index);
-      if (newChild === oldChild && sizeIndex === node.length - 1) {
-        return node;
-      }
-    }
-
-    var editable = ensureEditable(node, ownerID);
-    editable.splice(sizeIndex + 1);
-    if (newChild) {
-      editable[sizeIndex] = newChild;
-    }
-    return editable;
-}
-
-// defaults for testing
-function ensureEditable(node,id) {
-    return node;
-}
-
-function newTopFn(id) {
-    return new Array(BRANCHING);
 }
