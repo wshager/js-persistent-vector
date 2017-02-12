104a5656a79b88e7816e0fb1facac5b8715ff6db
W.S. Hager <wshager@gmail.com>
2017-02-12 21:28:06 +0100

    added iter stuffs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { PVec } from "./pvec";
 import { TVec } from "./tvec";
+import { values, entries } from "./shared";
 
 TVec.prototype.asPersistent = function (){
 	this.id = null;
@@ -9,6 +10,21 @@ PVec.prototype.asTransient = function () {
 	return new TVec(this.origin, this.capacity, this.shift, this.root, this.tail);
 };
 
+PVec.prototype.concat = function(other){
+    return this.asTransient().concat(other).asPersistent();
+};
+
+PVec.prototype.slice = function(begin,end){
+    return this.asTransient().slice(begin,end).asPersistent();
+};
+
+PVec.prototype[Symbol.iterator] = TVec.prototype[Symbol.iterator] = PVec.prototype.values = TVec.prototype.values = function(){
+    return values(this);
+};
+
+PVec.prototype.entries = TVec.prototype.entries = function(){
+    return entries(this);
+};
 
 export const empty = new PVec();
 export function isPVec(vec){
